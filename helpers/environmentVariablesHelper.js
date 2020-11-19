74ae8b28b60fc6000b11986e69e626cecf290167
nani3592 <venkatkadiveti248@gmail.com>
2020-11-19 14:32:34 +0530

    nodebb middleware

---
+++ helpers/environmentVariablesHelper.js
@@ -0,0 +1,26 @@
+'use strict'
+const env = process.env
+const __envIn = 'dev';
+// const __envIn = 'staging';
+// const __envIn = 'preprod';
+
+let devEnvVariables = {
+    DISCUSSION_FORUM: env.sunbird_discussion_forum || 'http://localhost:4567/api',
+}
+
+let stagingEnvVariables = {
+    DISCUSSION_FORUM: env.sunbird_discussion_forum || 'http://localhost:4567/api',
+}
+
+let preprodEnvVariables = {
+    DISCUSSION_FORUM: env.sunbird_discussion_forum || 'http://localhost:4567/api',
+}
+
+
+if (__envIn == 'dev') {
+    module.exports = devEnvVariables
+} else if (__envIn == 'staging') {
+    module.exports = stagingEnvVariables
+} else if (__envIn == 'preprod') {
+    module.exports = preprodEnvVariables
+}
